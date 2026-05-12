1f067641f412cc4b02d6a77ba5ea9fd6208674e0
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-05-11 19:51:50 -0500

    It allows you to open the menu modal to create menus, but they cannot be created yet.

--- src/app/modules/menu/components/menu-for-modal/menu-for-modal.ts
+++ src/app/modules/menu/components/menu-for-modal/menu-for-modal.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-for-modal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu-for-modal.html',
   styles: ``,
 })
 export class MenuForModal {
+  @Input() isVisible = false;
+  @Output() close = new EventEmitter<void>();
 
+  onClose(): void {
+    this.close.emit();
+  }
 }
--- src/app/modules/menu/pages/menu-list.ts
+++ src/app/modules/menu/pages/menu-list.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { MenuForModal } from '../components/menu-for-modal/menu-for-modal';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [MenuForModal],
   templateUrl: './menu-list.html',
   styles: ``,
 })
 export class Menu {
     showFormModal = signal(false);
-
-    
 }
