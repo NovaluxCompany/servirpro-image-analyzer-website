b9c054bd59e4bb7eaa6db1167fe09f2c928c7dea
JaiderNicolas <jaidernicolassierra0607@gmail.com>
2026-06-11 15:57:41 -0500

    generated the update company component

---
+++ src/app/modules/update-company/pages/update-company-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-company',
+  imports: [],
+  templateUrl: './update-company-list.html',
+  styles: ``,
+})
+export class UpdateCompanyList {
+
+}
